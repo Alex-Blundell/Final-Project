c92bec3618e0bfda028d64b45db1768a5da41709
Alex Blundell <alex.william.blundell@gmail.com>
2023-09-07 15:45:05 +1200

    Initial Project Upload.

---
+++ Final Project Web Application/Final Project Web Application/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Final_Project_Web_Application.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Final_Project_Web_Application.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Final Project Web Application/Final Project Web Application/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Final_Project_Web_Application.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
