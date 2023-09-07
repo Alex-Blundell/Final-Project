3c2395480236a5656a59692346da31d018ac8ed6
Alex Blundell <alex.william.blundell@gmail.com>
2023-09-07 15:53:12 +1200

    Added Base Class Details.

--- Final Project Web Application/Final Project Web Application/Models/Book.cs
+++ Final Project Web Application/Final Project Web Application/Models/Book.cs
@@ -2,5 +2,14 @@
 {
     public class Book
     {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Author { get; set; }
+
+        public Book()
+        {
+
+        }
     }
 }
--- Final Project Web Application/Final Project Web Application/Models/User.cs
+++ Final Project Web Application/Final Project Web Application/Models/User.cs
@@ -1,6 +1,29 @@
-﻿namespace Final_Project_Web_Application.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Final_Project_Web_Application.Models
 {
     public class User
     {
+        public enum SecurityLevel
+        {
+            User,
+            Admin,
+            Owner
+        }
+
+        [Key] public int Id { get; set; }
+        [Required] public string Username { get; set; }
+        [Required] public string Password { get; set; }
+        [Required] public SecurityLevel SecLevel { get; set; }
+
+        public string EncryptString()
+        {
+            return "";
+        }
+
+        public string DecryptString()
+        {
+            return "";
+        }
     }
 }
