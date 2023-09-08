b1dea1364722919772bc61761ec25030da10808f
Alex Blundell <alex.william.blundell@gmail.com>
2023-09-08 22:50:29 +1200

    Forum Update.

--- Final Project Web Application/Final Project Web Application/Models/Forum.cs
+++ Final Project Web Application/Final Project Web Application/Models/Forum.cs
@@ -2,5 +2,12 @@
 {
     public class Forum
     {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int ThreadsCount { get; set; }
+        public int ViewsCount { get; set; }
+        public int LastUserID { get; set; }
+        public string LastThreadTitle { get; set; }
+        public string LastThreadUpdate { get; set; }
     }
 }
