88b8015d1f84d123a3f899a063ba953772821787
Alex Blundell <alex.william.blundell@gmail.com>
2023-09-19 21:37:36 +1200

    Latest Update.

--- Final Project Web Application/Final Project Web Application/Controllers/ProfileController.cs
+++ Final Project Web Application/Final Project Web Application/Controllers/ProfileController.cs
@@ -7,6 +7,8 @@ namespace Final_Project_Web_Application.Controllers
         public IActionResult Index()
         {
             string IsDarkModeCookie = Request.Cookies["IsDarkMode"];
+            string UserID = Request.Cookies["UserID"];
+            string HasLoggedIn = Request.Cookies["HasLoggedIn"];
 
             // Probably the First time the Website has been Run, Add Cookie for Dark Mode and Set it to the Default Value.
             if (IsDarkModeCookie == null)
@@ -16,6 +18,12 @@ namespace Final_Project_Web_Application.Controllers
 
                 IsDarkModeCookie = "No";
                 Response.Cookies.Append("IsDarkMode", IsDarkModeCookie, Options);
+            }
+
+            if (HasLoggedIn == "Yes")
+            {
+                TempData["HasLoggedIn"] = HasLoggedIn;
+                TempData["UserID"] = UserID;
             }
 
             TempData["IsDarkMode"] = IsDarkModeCookie;
